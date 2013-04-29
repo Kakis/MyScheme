c868b4c5b6003717bc06be2c252a1cae1bd178ce
Jeppe_07 <jens.hagfeldt@gmail.com>
2013-04-29 14:25:24 +0200

    Initial Commit

---
+++ MyScheme/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MyScheme
+//
+//  Created by Jens Hagfeldt on 2013-04-29.
+//  Copyright (c) 2013 Jens Hagfeldt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
